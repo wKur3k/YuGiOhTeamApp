d451f77bcf2218deb80c16ee781ecb27c4a3623f
Wojciech Kurek <kurek.wojciech.01@gmail.com>
2022-03-25 17:06:08 +0100

    Added Database Seeder which is called at the startup

---
+++ YuGiOhTeamApp/DbSeeder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YuGiOhTeamApp.Entities;
+
+namespace YuGiOhTeamApp
+{
+    public class DbSeeder
+    {
+       private readonly AppDbContext _dbContext;
+        public DbSeeder(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.Roles.Any())
+                {
+                    var roles = GetRoles();
+                    _dbContext.Roles.AddRange(roles);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+        private IEnumerable<Role> GetRoles()
+        {
+            var roles = new List<Role>()
+            {
+                new Role()
+                {
+                    Name = "User"
+                },
+                new Role()
+                {
+                    Name = "Admin"
+                }
+            };
+            return roles;
+        }
+    }
+}
--- YuGiOhTeamApp/Program.cs
+++ YuGiOhTeamApp/Program.cs
@@ -1,13 +1,21 @@
+using YuGiOhTeamApp;
+using YuGiOhTeamApp.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddScoped<DbSeeder>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<DbSeeder>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+seeder.Seed();
 
 app.UseHttpsRedirection();
 
